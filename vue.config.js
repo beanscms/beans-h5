7425e3ac369085d64f392b56e5bb5e391061a5a2
xiaofuyesnew <xiaofuyesnew@qq.com>
2020-04-21 14:02:59 +0800

    research: vue config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  
+  // publicPath: process.env.NODE_ENV === 'production' ? '/' : './',
+  pages: {
+    admin: {
+      entry: 'src/pages/admin/main.js',
+      template: 'public/admin.html',
+      filename: 'admin.html',
+      title: 'Beans H5 Admin',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    h5: {
+      entry: 'src/pages/h5/main.js',
+      template: 'public/h5.html',
+      filename: 'h5.html',
+      title: 'Beans H5',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    }
+  }
 }
